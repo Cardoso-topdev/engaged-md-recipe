e9ddfacf1711dc5866ae141366af6a4630370449
cardoso-topdev <oscar@cardoso-devstar.com>
2022-05-26 09:40:30 -0500

    built mock server with json-server node module

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
 import MainRoute from './routes/MainRoute';
 import { theme } from './theme/theme.config';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
       <BrowserRouter>
         <MainRoute />        
       </BrowserRouter>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,11 @@
 import Box from '@mui/material/Box';
 import React, { PropsWithChildren } from 'react';
+import Header from '../components/Header';
 
 const MainLayout: React.FC<PropsWithChildren<{}>> = ({children}) => {
   return (
     <Box component={'div'}>
+      <Header />
       <Box m={10}>
         {children}
       </Box>
